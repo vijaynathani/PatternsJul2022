392d47fa24d64393949ba795ad0120782638e226
Vijay Nathani <vijay_nathani@yahoo.com>
2022-06-29 09:38:50 +0530

    UML Class diagram slides

--- Trng/Demo1/Class1.cs
+++ Trng/Demo1/Class1.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo1
+namespace Demo1;
+internal class Class1
 {
-    internal class Class1
+    public void test()
     {
-        public void test()
-        {
-        }
     }
 }
