714767cbc62f4f906bbce9c8d308bd8de4fe58ca
Vijay Nathani <vijay_nathani@yahoo.com>
2022-07-01 09:50:24 +0530

    S94

---
+++ Trng/Demo1/S94.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1;
+class Course
+{
+    string courseId;
+    string title;
+    int fee;
+    Session[] sessions;
+}
+class Session
+{
+    DateOnly date;
+    int startHour;
+    int endHour;
+}
+interface ICourseTables
+{
+    void Add(CourseTable a);
+    CourseTable Get(string courseId);
+    IEnumerable<CourseTable> GetAllCoursesOfHigherDuration(int durationHours);
+}
+
+class CourseTable
+{
+    string courseId;
+    string title;
+    int fee;
+}
+class SessionTable
+{
+    string courseId;
+    int sessionId;
+    DateOnly date;
+    int startHour, endHour;
+}
