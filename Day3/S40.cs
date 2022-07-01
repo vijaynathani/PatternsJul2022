35ce8ea112d6e173171f201172c7473bd0b3e0d6
Vijay Nathani <vijay_nathani@yahoo.com>
2022-07-01 09:17:51 +0530

    Create S40.cs

---
+++ Day3/S40.cs
@@ -0,0 +1,51 @@
+class Date { //..
+	public int getTime() { //...
+	}
+}
+
+interface Schedule {
+    int getDurationInDays();
+    void print();
+}
+class WeeklySchedule : Schedule {
+    int noWeeks;
+    Date fromDate;
+    Date toDate;
+    public int getDurationInDays() {
+        return noWeeks;
+    }
+    public void print() {
+        //...
+    }
+}
+class RangeSchedule : Schedule {
+    Date fromDate;
+    Date toDate;
+    public int getDurationInDays() {
+        int milliSecondsInOneDay = 24*60*60*1000;
+        return (int)((toDate.getTime()-fromDate.getTime()) /
+                    milliSecondsInOneDay);
+    }
+    public void print() {
+        //...
+    }
+}
+class ListSchedule : Schedule {
+    Date[] dateList;
+    public int getDurationInDays() {
+        return dateList.Length;
+    }
+    public void print() {
+    	//...
+    }
+}
+class Course {
+    string courseTitle;
+    Schedule schedule;
+    int getDurationInDays() {
+        return schedule.getDurationInDays();
+    }
+    void printSchedule() {
+        schedule.print();
+    }
+}
