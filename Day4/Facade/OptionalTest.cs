8681ffeb9bee0c55a1ba98caa36122624cd48fef
Vijay Nathani <vijay_nathani@yahoo.com>
2022-07-04 11:59:54 +0530

    Optional

---
+++ Day4/Facade/OptionalTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OOADandPatterns.OOAD
+{
+    public class OptionalTest
+    {
+        public static void Main1()
+        {
+            var a = Optional<string>.Of("abc");
+            var b = Optional<string>.Of("abc");
+            Debug.Assert(a.Equals(b));
+            var c = Optional<string>.Empty();
+            Debug.Assert(!a.Equals(c));
+            Debug.Assert(!c.Equals(Optional<string>.Empty()));
+            var d = Optional<string>.Of("xyz");
+            Debug.Assert(!d.Equals(a));
+            Debug.Assert(a.Get().Equals("abc"));
+            Debug.Assert(a.GetHashCode() > 1);
+            Debug.Assert(c.GetHashCode() == 1);
+            Debug.Assert(a.IsPresent());
+            Debug.Assert(!c.IsPresent());
+            var n = 1;
+            a.IsPresent(s => n = s.Length);
+            Debug.Assert(n == 3);
+            n = 0;
+            c.IsPresent(s => n = 5);
+            Debug.Assert(n == 0);
+            Debug.Assert(Optional<string>.OfNullable(null).GetHashCode() == 1);
+            Debug.Assert(a.OrElse("xyz").Equals("abc"));
+            Debug.Assert(c.OrElse("xyz").Equals("xyz"));
+            Debug.Assert(a.OrElseGet(() => "pqr").Equals("abc"));
+            Debug.Assert(c.OrElseGet(() => "pqr").Equals("pqr"));
+            a.OrElseThrow(() => new Exception());
+            n = 3;
+            Debug.Assert(a.Filter(s => s.Length == 3).Equals(a));
+            Debug.Assert(!a.Filter(s => s.Length == 0).IsPresent());
+            Debug.Assert(!c.Filter(s => true).IsPresent());
+            var g = new StringBuilder("a");
+            var f = Optional<StringBuilder>.Of(g);
+            Debug.Assert(a.Map(s => g).Get().Equals(g));
+            Debug.Assert(!c.Map(s => g).IsPresent());
+            Debug.Assert(a.FlatMap(s => f).Equals(f));
+           Debug.Assert(!c.FlatMap(s => f).IsPresent());
+            
+
+        }
+    }
+}
---
+++ Trng/Demo1/Adapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trng.Demo1
+{
+    class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+    }
+    internal class Adapter
+    {
+        private readonly Rectangle r;
+        public Adapter(Rectangle r1) => r = r1;
+        public void setHeightAndWidth(int width, int height)
+        {
+            r.Height = height;
+            r.Width = width;
+        }
+    }
+    class AdapterMain
+    {
+        public void Main1()
+        {
+            Rectangle r = new Rectangle();
+            var a = new Adapter(r);
+            a.setHeightAndWidth(20, 50);
+        }
+    }
+}
