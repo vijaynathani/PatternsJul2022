ea5396e805d4857a319f8858befc7703c9ab82cb
Vijay Nathani <vijay_nathani@yahoo.com>
2022-07-04 12:03:37 +0530

    Q28

---
+++ Day4/Facade/Q28Artists.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnCS.ooad
+{
+    internal class Q28Artists
+    {
+        private readonly List<Artist> _artists;
+
+        public Q28Artists(List<Artist> artists)
+        {
+            _artists = artists;
+        }
+
+        public Artist GetArtist(int index)
+        {
+            if ((index < 0) || (index >= _artists.Count)) return null;
+            return _artists[index];
+        }
+
+        public String GetArtistName(int index)
+        {
+            try
+            {
+                return GetArtist(index).GetName();
+            }
+            catch (NullReferenceException e)
+            {
+                return null;
+            }
+        }
+
+        //Improve the design of this class
+    }
+
+
+    internal class Artist
+    {
+        private readonly String _name;
+
+        public Artist(String name)
+        {
+            _name = name;
+        }
+
+        public String GetName()
+        {
+            return _name;
+        }
+    }
+}
---
+++ Day4/Facade/S28Artists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnCS.ooad
+{
+    internal class S28Artists
+    {
+        private readonly List<Artist> _artists;
+
+        public S28Artists(List<Artist> artists)
+        {
+            _artists = artists;
+        }
+
+        public Artist GetArtist(int index)
+        {
+            if ((index < 0) || (index >= _artists.Count))
+                throw new ArgumentException(index + " doesn't correspond to an Artist");
+            return _artists[index];
+        }
+
+        public String GetArtistName(int index)
+        {
+             return GetArtist(index).GetName(); //Throw exception if invalid index.
+        }
+    }
+}
