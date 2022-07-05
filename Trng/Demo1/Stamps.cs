662ee5dedc8e57d1a141bb05399c6b6916b48c1b
Vijay Nathani <vijay_nathani@yahoo.com>
2022-07-05 12:11:49 +0530

    Create Stamps.cs

---
+++ Trng/Demo1/Stamps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trng.Demo1
+{
+    class HasCents
+    {
+        int value;
+        public void Add(int coinAdded)
+        {
+            value += coinAdded;
+            if (value >= 20)
+            {
+                Console.WriteLine("Stamp dispensed");
+                value -= 20;
+                Cancel();
+            }
+        }
+        public void Cancel()
+        {
+            if (value == 0) return;
+            Console.WriteLine("Returned {0} coins", value);
+            value = 0;
+        }
+
+    }
+    internal class Stamps
+    {
+        HasCents hc = new HasCents();
+        public void FiveCentsInserted() => hc.Add(5);
+        public void TenCentsInserted() => hc.Add(10);
+        public void ReturnPressed() => hc.Cancel();
+    }
+}
